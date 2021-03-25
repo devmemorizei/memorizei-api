a47948960c3d3db61dbc6483e33471626451a61c
jonas.fonseca <dornelasjonas96@hotmail.com>
2021-03-25 20:43:41 -0300

    voltando as configs de email ao normal

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,9 +2,8 @@ import nodemailer from 'nodemailer';
 
 let transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
-    ignoreTLS: false,
-    secure: false, // true for 465, false for other ports
+    port: 465,
+    secure: true, // true for 465, false for other ports
     auth: {
       user: process.env.EMAIL,
       pass: process.env.PASSWORD,
