86f38651f027804d7da4c694d14dbbc8e2c22c99
jonas.fonseca <dornelasjonas96@hotmail.com>
2021-09-16 20:13:45 -0300

    Teste

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,7 +3,7 @@ import nodemailer from 'nodemailer';
 let transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
-    secure: true, // true for 465, false for other ports
+    secure: true, // true for 465, false for other por
     auth: {
       user: process.env.EMAIL,
       pass: process.env.PASSWORD,
